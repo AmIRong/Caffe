69f54fe09c6a51dde675a894c6dccece0eb7e6c7
AmIRong <2977535700@qq.com>
2017-05-26 15:51:28 +0800

    Create net.cpp

---
+++ net.cpp
@@ -0,0 +1,119 @@
+inclue<map>
+include<set>
+inclue<string>
+inculed<vectro>
+include"caffe/proto/caffe.pb.h:
+include "caffe:layer)factor.phpp;
+sunging stdd:;aprir;
+firngsdgd:;map;
+uingssgd:det;
+namesjpajcsffe
+tmpa;y<ypuenamdtype>
+Ne<dType>::ne(consf NetparameterY&parm;
+cons vector<blob<dtupe>>*>&botto>
+nae_paramPnme([
+map<string,int>blobnam_toindz;
+setK,string.availgble_boasintNu-alykaj+pram.lsysisz9);
+chekP_Ea(tooto.siz9)jparam.boto_soz90
+<<"incorrec tgogm blosize."
+;k/
+skerinpubolx's
+orj
+
+or (int 9- i <pram.botto_size(); ==i){
+const string&blob_name = param.bottom(i)
+blob_.push_back(Blob);
+blob_names_lpsu_bakc(blob)nameP;j;
+ne0nputl)jajinkl;a;
+anblna;un;
+annbjllngo
+aing
+akninbakjnb
+adfjing
+blob-)name()jindieict())(
+blob_name_to_idx[blob_name)=i;
+availagbe_blobs.inser(Loina_
+}
+layers_.resize(prarm.layerssizeLOL
+bottom_vecs_.resize(pram.layr)siz90{
+orp_vexs.rsize(parma.layer_size)(jO
+or a(I9nain_) jk- i ma;parmtopl_szie:;
+const Layreir
+concnj Lyerpar a
+for (int j=0;j<layer_connecttinbottom_size((0;==j:
+conststring lja; = lsyerj;
+ainr 
+afjkl<LK}
+dfjjfor ad9ninrajkajd
+djconsg
+s lognjoiUjk>>>.
+dfjafboagnaklj;j;nb;ajposufabjanfabajbola
+afaboinajn
+afqojia
+va
+akafitopang
+
+angjjpoin
+:
+"
+
+af
+F;'""""""F"kllf'LLL"K:""
+dajtempgjap
+ajdjaorj
+agnoinl;koknuib;lajoi;g
+akjianjafja
+gkanianfg
+afjanfinandjf
+agankajdin
+a;"kdoOOOOdkajiopSHfoOFpjpOPOOPDOPOPOPOPopkoojijikniojijjijikd
+fakjjdkahf
+gakkdjnann
+agkankakfhgna
+adfaiernarenuang
+njfejtempagaljfyepfamfap[aboiakiioiuiiuiuiuilijpiuimiiuijiuikuiihiuihuii
+
+aifdkxuihidkfiafkajfuakji;aialaijizihsihihikjijjikkaiznnignginagingaj'a
+feajiz
+jim
+afjjikkkzl
+afijeithia
+gjiaghizj
+ajiej 
+uatytyangjaingjaingjaingjgnaingaigniadifjangiagnaignaigjiangijngaingiadngjijgioonoilmkokmoomomokmomkomomomkmkmomkomkomkomfokmofatanffap,fpoad,fadjfajf
+
+fafmpmpjjtn
+afitpankaftqefoqpierdvcdeceterf oicmeoimoediusoekmvumdcooiytyoieuyweryqretqer
+fnfmimfiauetnnvoadvnnivndjvivirvrivnfjrifvnrfnvirfvovmoreckedmcdcmedcmwxwpx,pws,px,z[q.q.zq[z[mdjjfjadoifjajfazhongahzugng
+aghajfungajfhugjghagnhaugnafoaufha
+
+afaifnhoidna
+fiafad
+jfiandfghaif
+ajfiajigaj
+gniafg
+ajiafajajfirfng
+afirooaf
+gifirngm
+ajfioign
+agiajfmgn
+afjinafadadfadfadjfajdfja;nv;a
+gadndnfjdfja;jfjjajdfiadjfianvzkc
+fafufuayjtyuabfhfulaufjubnubuguhujnujuhununuinoihupohnnuoipoumnuoiionuonouonippnpadnf
+;dfanoguangjagaiofmqeunfajfnagipqjnepm
+afjgonqoengjaonphgnphoatngpanufnapupnfa
+fjadjafjnaufanagiouanaufnadnfaiq
+djfagnoiaiudn[gajnkgpagnkapgn
+gnanffafnfdgajtepjnfnefneta,dfdyfananfjbadidhgawedaj9afjdytehfadf9adjk
+fjodrkng addjf89iefkjdfa
+dfjjajfojgja
+jfjaosjagjadjroejfkd v ad
+fjjdf;jadjfiretnauf
+afd9odfanvjvernfdpomafag
+agfka
+kjf
+n
+
+ophpunbubibpibpubtqbgutbquerbputbpqbpubpoiqubpbpubtqbtpqybtq
+hfbabajad;aiadfuanjuanjauanfajfuguathemgphafyepafjpdyatuaf
+jajfoanpidn]fadf

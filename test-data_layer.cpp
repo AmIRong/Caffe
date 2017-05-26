2b7c9d93d4764d39ee35beb6dbd56629dab71e5c
AmIRong <2977535700@qq.com>
2017-05-26 11:55:04 +0800

    Create test-data_layer.cpp

---
+++ test-data_layer.cpp
@@ -0,0 +1,8 @@
+blob_top_vec_.push-ack(blob_top_alvel_);
+//Create the leveldb
+filenam + tmpnam(NULL0;/get temp name 
+LOG(ERROR)<<"Using temporyaty leveldb"<<filename;
+Log(INFO)<<"Using tempory leveldb"<<filename;
+leveldb::DB*db;
+leveldb:options options;
+options.error_if_exists =true;
